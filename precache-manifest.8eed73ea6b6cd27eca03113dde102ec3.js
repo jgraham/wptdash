54fb6f43c61346a728cb63a8f54888a4ea35cd72
James Graham <james@hoppipolla.co.uk>
2019-02-19 16:15:18 +0000

    Updates

---
+++ precache-manifest.8eed73ea6b6cd27eca03113dde102ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "946e0fbb3b833d103240",
+    "url": "/wptdash/static/js/runtime~main.946e0fbb.js"
+  },
+  {
+    "revision": "0463b98bd3a655eab5fe",
+    "url": "/wptdash/static/js/main.0463b98b.chunk.js"
+  },
+  {
+    "revision": "3f88335e2b4a4b2cbd2f",
+    "url": "/wptdash/static/js/1.3f88335e.chunk.js"
+  },
+  {
+    "revision": "0463b98bd3a655eab5fe",
+    "url": "/wptdash/static/css/main.cb05b7a3.chunk.css"
+  },
+  {
+    "revision": "0f9d0e7f79dc214a0171433ab55f5059",
+    "url": "/wptdash/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wptdash/precache-manifest.605f2a7996d165a1c28fd2faa63adcc5.js"
+  "/wptdash/precache-manifest.8eed73ea6b6cd27eca03113dde102ec3.js"
 );
 
 workbox.clientsClaim();

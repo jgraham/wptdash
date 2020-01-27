3538033b20110408b217aee6aec17736bba68b21
James Graham <james@hoppipolla.co.uk>
2020-01-27 21:50:20 +0000

    Updates

---
+++ precache-manifest.0ab6b6f1ee6342f664fd4c3633b91835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d741c53d443935888eae3ec33b1317",
+    "url": "/wptdash/index.html"
+  },
+  {
+    "revision": "47baf93136b94e2554c6",
+    "url": "/wptdash/static/css/main.562176a1.chunk.css"
+  },
+  {
+    "revision": "206b3a3db6bc49605d38",
+    "url": "/wptdash/static/js/2.cdafaca4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wptdash/static/js/2.cdafaca4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47baf93136b94e2554c6",
+    "url": "/wptdash/static/js/main.da77fca0.chunk.js"
+  },
+  {
+    "revision": "75f740a38cabbc081555",
+    "url": "/wptdash/static/js/runtime-main.c57754c7.js"
+  }
+]);
